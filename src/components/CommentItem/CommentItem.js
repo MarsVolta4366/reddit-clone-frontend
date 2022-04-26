923bf8c8f8d7ef8ccb8c6670641ecf999e3c5cba
unknown <derek.slauson5@gmail.com>
2022-04-25 19:29:31 -0500

    Comment edit functionality is working, still fixing slightly unwanted behavior

--- src/components/CommentItem/CommentItem.js
+++ src/components/CommentItem/CommentItem.js
@@ -1,5 +1,5 @@
 import { Delete, Edit, MoreHoriz, PersonOutline } from "@mui/icons-material"
-import { Button, Menu, MenuItem } from "@mui/material"
+import { Button, Menu, MenuItem, TextareaAutosize } from "@mui/material"
 import { useContext, useState } from "react"
 import { CurrentUserContext } from "../../context/CurrentUserContext"
 
@@ -8,6 +8,8 @@ const CommentItem = ({ comment, post_id }) => {
     const { currentUser } = useContext(CurrentUserContext)
     const [anchorCommentActions, setAnchorCommentActions] = useState(null)
     const commentActionsOpen = Boolean(anchorCommentActions)
+    const [toggleCommentEdit, setToggleCommentEdit] = useState(false)
+    const [editedComment, setEditedComment] = useState({ comment_id: comment.comment_id, text: comment.text })
 
     const deleteComment = async (comment) => {
         await fetch("http://localhost:4000/comments", {
@@ -36,12 +38,12 @@ const CommentItem = ({ comment, post_id }) => {
                             open={commentActionsOpen}
                             onClose={() => setAnchorCommentActions(null)}>
                             <MenuItem variant="darkMenuItemLeft" onClick={() => {
+                                setToggleCommentEdit(!toggleCommentEdit)
                                 setAnchorCommentActions(null)
                             }}><Edit className="darkIcon" />Edit</MenuItem>
                             <MenuItem variant="darkMenuItemLeft" onClick={() => {
                                 deleteComment(comment)
                                 setAnchorCommentActions(null)
-                                // window.location.href = `/comments/${post_id}`
                             }}><Delete className="darkIcon" />Delete</MenuItem>
                         </Menu>
                     </form>
@@ -50,13 +52,43 @@ const CommentItem = ({ comment, post_id }) => {
         }
     }
 
+    const updateComment = async () => {
+        await fetch("http://localhost:4000/comments", {
+            method: "PUT",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(editedComment)
+        })
+    }
+
+    let commentEditForm = (
+        <form onSubmit={updateComment}>
+            <TextareaAutosize
+                maxLength={1000}
+                placeholder="Text (optional)"
+                className="darkBackground whiteText"
+                value={editedComment.text}
+                onChange={(e) => setEditedComment({ ...editedComment, text: e.target.value })}
+                style={{
+                    padding: "10px",
+                    width: "96%",
+                    resize: "vertical",
+                    height: "200px"
+                }} />
+            <Button className="lightTextButton" onClick={() => setToggleCommentEdit(false)}>Cancel</Button>
+            <Button type="submit" className="grayButton">Save</Button>
+        </form>
+    )
+
     return (
         <div className="whiteText commentBox">
             <div className="iconAndTextBox">
                 <PersonOutline className="darkIcon" style={{ margin: "0" }} />
                 <h2 className="commentUsername">{comment.User.username}</h2>
             </div>
-            <pre className="commentText">{comment.text}</pre>
+            {toggleCommentEdit ? commentEditForm : <pre className="commentText">{comment.text}</pre>}
             {renderCommentActions(comment)}
         </div>
     )
